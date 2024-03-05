4f429ec1be2bd27881b68ead9c09ff49cc1c213d
Miguel Daipré <migueldaipre@gmail.com>
2024-03-04 21:07:10 -0300

    feat: add siteConfig

---
+++ src/config/site.ts
@@ -0,0 +1,15 @@
+interface SiteConfig {
+  title: string
+  description: string
+  language: string
+  imageOg: string
+  author: string
+}
+
+export const siteConfig: SiteConfig = {
+  title: 'Miguel Daipré',
+  description: 'I am a Senior React Native Developer and OSS Contributor',
+  language: 'pt',
+  imageOg: 'og.jpg',
+  author: 'Miguel Daipré',
+}
