971a1674c3331e696361185277efbef81ee8d898
Miguel Daipré <migueldaipre@gmail.com>
2024-03-05 00:23:17 -0300

    feat: configure vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import UnoCSS from 'unocss/astro';
+import vercel from '@astrojs/vercel/serverless';
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    UnoCSS({ 
-      injectReset: true,
-    }),
-  ],
+  integrations: [UnoCSS({
+    injectReset: true
+  })],
+  output: 'server',
+  adapter: vercel()
 });
