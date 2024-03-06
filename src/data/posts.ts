43d01aa68c2ba95ecc95537584f09f562c25dd84
Miguel Daipré <migueldaipre@gmail.com>
2024-03-05 23:17:49 -0300

    feat: add is draft filter

--- src/data/posts.ts
+++ src/data/posts.ts
@@ -1,6 +1,7 @@
 import { getCollection } from 'astro:content'
 
 export const posts = (await getCollection('posts'))
+  .filter(post => !post.data.isDraft)
   .sort((a, b) => b.data.publishedAt.valueOf() - a.data.publishedAt.valueOf())
   .map(post => ({ ...post.data, slug: post.slug }))
 
