a20924840000236ad996f79b630dce31150a135f
Miguel Daipré <miguel.daipre@totvs.com.br>
2024-06-25 21:55:39 -0300

    chore: upgrade dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,26 @@ import vercel from '@astrojs/vercel/serverless'
 import { defineConfig } from 'astro/config'
 import UnoCSS from 'unocss/astro'
 
+import prettyCode from 'rehype-pretty-code'
+const prettyCodeOptions = {
+	theme: 'rose-pine-moon',
+}
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://migueldaipre.dev',
 	i18n: {
 		defaultLocale: 'pt-BR',
-		locales: ['en', 'pt-BR'],
+		locales: ['pt-BR', 'en'],
 	},
 	integrations: [
 		UnoCSS({
 			injectReset: true,
 		}),
-		mdx(),
+		mdx({
+			syntaxHighlight: false,
+			rehypePlugins: [[prettyCode, prettyCodeOptions]],
+		}),
 	],
 	output: 'server',
 	adapter: vercel({
--- src/data/posts.ts
+++ src/data/posts.ts
@@ -1,8 +1,10 @@
 import { getCollection } from 'astro:content'
 
-export const posts = (await getCollection('posts'))
-	.filter(post => !post.data.isDraft)
-	.sort((a, b) => b.data.publishedAt.valueOf() - a.data.publishedAt.valueOf())
-	.map(post => ({ ...post.data, slug: post.slug }))
+export const getPosts = async () =>
+	(await getCollection('posts'))
+		.filter(post => !post.data.isDraft)
+		.sort((a, b) => b.data.publishedAt.valueOf() - a.data.publishedAt.valueOf())
+		.map(post => ({ ...post.data, slug: post.slug }))
 
-export const latestPosts = (limit: number) => posts.slice(0, limit)
+export const latestPosts = async (limit: number) =>
+	(await getPosts()).slice(0, limit)
