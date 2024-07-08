a3069e1d07a34283040233cc5d868dcded1051f7
Miguel Daipré <miguel.daipre@totvs.com.br>
2024-07-08 15:29:06 -0300

    feat: configure sitemap and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,38 @@
-import mdx from '@astrojs/mdx'
-import vercel from '@astrojs/vercel/serverless'
-import { defineConfig } from 'astro/config'
-import UnoCSS from 'unocss/astro'
+import mdx from '@astrojs/mdx';
+import vercel from '@astrojs/vercel/serverless';
+import sitemap from '@astrojs/sitemap';
 
-import prettyCode from 'rehype-pretty-code'
+import { defineConfig } from 'astro/config';
+import UnoCSS from 'unocss/astro';
+import prettyCode from 'rehype-pretty-code';
+
 const prettyCodeOptions = {
-	theme: 'rose-pine-moon',
-}
+  theme: 'rose-pine-moon',
+};
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://migueldaipre.dev',
-	i18n: {
-		defaultLocale: 'pt-br',
-		locales: ['pt-br', 'en'],
-		routing: {
-			redirectToDefaultLocale: true,
-			prefixDefaultLocale: true,
-		}
-	},
-	integrations: [
-		UnoCSS({
-			injectReset: true,
-		}),
-		mdx({
-			syntaxHighlight: false,
-			rehypePlugins: [[prettyCode, prettyCodeOptions]],
-		}),
-	],
-	output: 'server',
-	adapter: vercel({
-		imageService: true,
-	}),
-})
+  site: 'https://migueldaipre.dev',
+  i18n: {
+    defaultLocale: 'pt-br',
+    locales: ['pt-br', 'en'],
+    routing: {
+      redirectToDefaultLocale: true,
+      prefixDefaultLocale: true,
+    },
+  },
+  integrations: [
+    UnoCSS({
+      injectReset: true,
+    }),
+    mdx({
+      syntaxHighlight: false,
+      rehypePlugins: [[prettyCode, prettyCodeOptions]],
+    }),
+    sitemap(),
+  ],
+  output: 'server',
+  adapter: vercel({
+    imageService: true,
+  }),
+});
