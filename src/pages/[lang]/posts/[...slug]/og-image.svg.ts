2001a39eead7dd5dec7f5426a39c1917d1bc5535
Miguel Daipré <miguel.daipre@totvs.com.br>
2024-07-09 20:25:40 -0300

    fix: prerender

--- src/pages/[lang]/posts/[...slug]/og-image.svg.ts
+++ src/pages/[lang]/posts/[...slug]/og-image.svg.ts
@@ -3,15 +3,19 @@ import { renderOG } from '@utils/renderOG'
 
 import type { APIRoute } from 'astro'
 
-//export const prerender = true
+export const prerender = true
 
-// export async function getStaticPaths() {
-// 	const posts = await getCollection('posts')
-// 	return posts.map(post => ({
-// 		params: { slug: post.slug, lang: post.data.lang },
-// 		props: post,
-// 	}))
-// }
+export async function getStaticPaths() {
+	const posts = await getCollection('posts')
+	return posts.map(post => {
+		const [lang, ...slug] = post.slug.split('/')
+
+		return {
+			params: { lang, slug: slug.join('/') || undefined },
+			props: { post },
+		}
+	})
+}
 
 export const GET: APIRoute = async function get({ params }) {
 	const post = await getEntry('posts', `${params.lang}/${params.slug}`)
